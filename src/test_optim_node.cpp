d518d2bd2814cd2c263bf2bae55f3551ea56961e
DinnerHowe <xu001314@163.com>
2019-10-24 00:17:46 +0800

    change code type

--- src/test_optim_node.cpp
+++ src/test_optim_node.cpp
@@ -1,41 +1,3 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2016,
- *  TU Dortmund - Institute of Control Theory and Systems Engineering.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the institute nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Christoph Rösmann
- *********************************************************************/
-
 #include <teb_local_planner/teb_local_planner_ros.h>
 
 #include <interactive_markers/interactive_marker_server.h>
@@ -45,10 +7,8 @@
 #include <boost/make_shared.hpp>
 
 
-using namespace teb_local_planner; // it is ok here to import everything for testing purposes
+using namespace teb_local_planner;
 
-// ============= Global Variables ================
-// Ok global variables are bad, but here we only have a simple testing node.
 PlannerInterfacePtr planner;
 TebVisualizationPtr visual;
 std::vector<ObstaclePtr> obst_vector;
@@ -59,7 +19,6 @@ ros::Subscriber custom_obst_sub;
 ros::Subscriber clicked_points_sub;
 unsigned int no_fixed_obstacles;
 
-// =========== Function declarations =============
 void CB_mainCycle(const ros::TimerEvent& e);
 void CB_publishCycle(const ros::TimerEvent& e);
 void CB_reconfigure(TebLocalPlannerReconfigureConfig& reconfig, uint32_t level);
@@ -69,79 +28,70 @@ void CB_obstacle_marker(const visualization_msgs::InteractiveMarkerFeedbackConst
 void CB_clicked_points(const geometry_msgs::PointStampedConstPtr& point_msg);
 
 
-// =============== Main function =================
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ) {
   ros::init(argc, argv, "test_optim_node");
   ros::NodeHandle n("~");
- 
-  
+
   // load ros parameters from node handle
   config.loadRosParamFromNodeHandle(n);
- 
+
   ros::Timer cycle_timer = n.createTimer(ros::Duration(0.025), CB_mainCycle);
   ros::Timer publish_timer = n.createTimer(ros::Duration(0.1), CB_publishCycle);
-  
+
   // setup dynamic reconfigure
-  dynamic_recfg = boost::make_shared< dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig> >(n);
+  dynamic_recfg = boost::make_shared<dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>>(n);
   dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>::CallbackType cb = boost::bind(CB_reconfigure, _1, _2);
   dynamic_recfg->setCallback(cb);
-  
+
   // setup callback for custom obstacles
   custom_obst_sub = n.subscribe("obstacles", 1, CB_customObstacle);
-  
+
   // setup callback for clicked points (in rviz) that are considered as via-points
   clicked_points_sub = n.subscribe("/clicked_point", 5, CB_clicked_points);
-  
+
   // interactive marker server for simulated dynamic obstacles
   interactive_markers::InteractiveMarkerServer marker_server("marker_obstacles");
 
-  obst_vector.push_back( boost::make_shared<PointObstacle>(-5,1) );
-  obst_vector.push_back( boost::make_shared<PointObstacle>(-5,2.2) );
-  obst_vector.push_back( boost::make_shared<PointObstacle>(0,0.1) );
-//  obst_vector.push_back( boost::make_shared<LineObstacle>(1,1.5,1,-1.5) ); //90 deg
-//  obst_vector.push_back( boost::make_shared<LineObstacle>(1,0,-1,0) ); //180 deg
-//  obst_vector.push_back( boost::make_shared<PointObstacle>(-1.5,-0.5) );
-  
-  /*
-  PolygonObstacle* polyobst = new PolygonObstacle;
-  polyobst->pushBackVertex(1, -1);
-  polyobst->pushBackVertex(0, 1);
-  polyobst->pushBackVertex(1, 1);
-  polyobst->pushBackVertex(2, 1);
- 
-  polyobst->finalizePolygon();
-  obst_vector.emplace_back(polyobst);
-  */
-  
-  for (unsigned int i=0; i<obst_vector.size(); ++i)
-  {
-    //CreateInteractiveMarker(obst_vector.at(i)[0],obst_vector.at(i)[1],i,&marker_server, &CB_obstacle_marker);  
+  obst_vector.push_back(boost::make_shared<PointObstacle>(-5,1));
+  obst_vector.push_back(boost::make_shared<PointObstacle>(-5,2.2));
+  obst_vector.push_back(boost::make_shared<PointObstacle>(0,0.1));
+
+  for (unsigned int i=0; i<obst_vector.size(); ++i) {
     // Add interactive markers for all point obstacles
     boost::shared_ptr<PointObstacle> pobst = boost::dynamic_pointer_cast<PointObstacle>(obst_vector.at(i));
-    if (pobst)
-    {
-      CreateInteractiveMarker(pobst->x(),pobst->y(),i, config.map_frame, &marker_server, &CB_obstacle_marker);  
+    if (pobst) {
+      CreateInteractiveMarker(pobst->x(),
+                              pobst->y(),
+                              i,
+                              config.map_frame,
+                              &marker_server,
+                              &CB_obstacle_marker);
     }
   }
   marker_server.applyChanges();
-  
-  
-  // Add via points
-  //via_points.push_back( Eigen::Vector2d( 0.0, 1.5 ) );
-  
+
   // Setup visualization
   visual = TebVisualizationPtr(new TebVisualization(n, config.map_frame));
-  
+
   // Setup robot shape model
   RobotFootprintModelPtr robot_model = TebLocalPlannerROS::getRobotFootprintFromParamServer(n);
-  
+
   // Setup planner (homotopy class planning or just the local teb planner)
-  if (config.hcp.enable_homotopy_class_planning)
-    planner = PlannerInterfacePtr(new HomotopyClassPlanner(config, &obst_vector, robot_model, visual, &via_points));
-  else
-    planner = PlannerInterfacePtr(new TebOptimalPlanner(config, &obst_vector, robot_model, visual, &via_points));
-  
+  if (config.hcp.enable_homotopy_class_planning) {
+    // use parallels planning
+    planner = PlannerInterfacePtr(new HomotopyClassPlanner(config,
+                                                           &obst_vector,
+                                                           robot_model,
+                                                           visual,
+                                                           &via_points));
+  } else {
+    // use single planning
+    planner = PlannerInterfacePtr(new TebOptimalPlanner(config,
+                                                        &obst_vector,
+                                                        robot_model,
+                                                        visual,
+                                                        &via_points));
+  }
 
   no_fixed_obstacles = obst_vector.size();
   ros::spin();
@@ -150,26 +100,28 @@ int main( int argc, char** argv )
 }
 
 // Planning loop
-void CB_mainCycle(const ros::TimerEvent& e)
-{
-  planner->plan(PoseSE2(-4,0,0), PoseSE2(4,0,0)); // hardcoded start and goal for testing purposes
+void CB_mainCycle(const ros::TimerEvent& e) {
+  planner->plan(PoseSE2(-4,0,0), PoseSE2(4,0,0));
 }
 
 // Visualization loop
-void CB_publishCycle(const ros::TimerEvent& e)
-{
+void CB_publishCycle(const ros::TimerEvent& e) {
   planner->visualize();
   visual->publishObstacles(obst_vector);
   visual->publishViaPoints(via_points);
 }
 
-void CB_reconfigure(TebLocalPlannerReconfigureConfig& reconfig, uint32_t level)
-{
+void CB_reconfigure(TebLocalPlannerReconfigureConfig& reconfig,
+                    uint32_t level) {
   config.reconfigure(reconfig);
 }
 
-void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigned int id, std::string frame, interactive_markers::InteractiveMarkerServer* marker_server, interactive_markers::InteractiveMarkerServer::FeedbackCallback feedback_cb)
-{
+void CreateInteractiveMarker(const double& init_x,
+                             const double& init_y,
+                             unsigned int id,
+                             std::string frame,
+                             interactive_markers::InteractiveMarkerServer* marker_server,
+                             interactive_markers::InteractiveMarkerServer::FeedbackCallback feedback_cb) {
   // create an interactive marker for our server
   visualization_msgs::InteractiveMarker i_marker;
   i_marker.header.frame_id = frame;
@@ -220,35 +172,29 @@ void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigne
   marker_server->setCallback(i_marker.name,feedback_cb);
 }
 
-void CB_obstacle_marker(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
-{
+void CB_obstacle_marker(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
   std::stringstream ss(feedback->marker_name);
   unsigned int index;
   ss >> index;
-  
-  if (index>=no_fixed_obstacles) 
+  if (index>=no_fixed_obstacles) {
     return;
+  }
   PointObstacle* pobst = static_cast<PointObstacle*>(obst_vector.at(index).get());
   pobst->position() = Eigen::Vector2d(feedback->pose.position.x,feedback->pose.position.y);	  
 }
 
-void CB_customObstacle(const teb_local_planner::ObstacleMsg::ConstPtr& obst_msg)
-{
+void CB_customObstacle(const teb_local_planner::ObstacleMsg::ConstPtr& obst_msg) {
   // resize such that the vector contains only the fixed obstacles specified inside the main function
   obst_vector.resize(no_fixed_obstacles);
-  
-  // Add custom obstacles obtained via message (assume that all obstacles coordiantes are specified in the default planning frame)  
-  for (std::vector<geometry_msgs::PolygonStamped>::const_iterator obst_it = obst_msg->obstacles.begin(); obst_it != obst_msg->obstacles.end(); ++obst_it)
-  {
-    if (obst_it->polygon.points.size() == 1 )
-    {
-      obst_vector.push_back(ObstaclePtr(new PointObstacle( obst_it->polygon.points.front().x, obst_it->polygon.points.front().y )));
-    }
-    else
-    {
+  // Add custom obstacles obtained via message (assume that all obstacles coordiantes are specified in the default planning frame)
+  for (std::vector<geometry_msgs::PolygonStamped>::const_iterator obst_it = obst_msg->obstacles.begin();
+        obst_it != obst_msg->obstacles.end(); ++obst_it) {
+    if (obst_it->polygon.points.size() == 1 ) {
+      obst_vector.push_back(ObstaclePtr(new PointObstacle( obst_it->polygon.points.front().x,
+                                                           obst_it->polygon.points.front().y )));
+    } else {
       PolygonObstacle* polyobst = new PolygonObstacle;
-      for (int i=0; i<(int)obst_it->polygon.points.size(); ++i)
-      {
+      for (int i=0; i<(int)obst_it->polygon.points.size(); ++i) {
         polyobst->pushBackVertex( obst_it->polygon.points[i].x, obst_it->polygon.points[i].y );
       }
       polyobst->finalizePolygon();
@@ -257,13 +203,12 @@ void CB_customObstacle(const teb_local_planner::ObstacleMsg::ConstPtr& obst_msg)
   }
 }
 
-
-void CB_clicked_points(const geometry_msgs::PointStampedConstPtr& point_msg)
-{
+void CB_clicked_points(const geometry_msgs::PointStampedConstPtr& point_msg) {
   // we assume for simplicity that the fixed frame is already the map/planning frame
   // consider clicked points as via-points
   via_points.push_back( Eigen::Vector2d(point_msg->point.x, point_msg->point.y) );
   ROS_INFO_STREAM("Via-point (" << point_msg->point.x << "," << point_msg->point.y << ") added.");
-  if (config.optim.weight_viapoint<=0)
+  if (config.optim.weight_viapoint<=0) {
     ROS_WARN("Note, via-points are deactivated, since 'weight_via_point' <= 0");
+  }
 }
